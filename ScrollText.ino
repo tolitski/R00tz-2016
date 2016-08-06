3383111ca63d8ea0ea6b893a08adc68d9be03020
Roselito de los Reyes <tolitski@hotmail.com>
2016-08-05 20:03:46 -0700

    Create ScrollText.ino

---
+++ ScrollText.ino
@@ -0,0 +1,95 @@
+/****************************************************************
+ScrollText.ino
+LED Array 8x7 Charlieplex
+Shawn Hymel @ SparkFun Electronics
+February 3, 2015
+https://github.com/sparkfun/LED_Array_8x7_Charlieplex
+Scrolls text across the LED array for 10 seconds.
+Hardware Connections:
+IMPORTANT:  The Charlieplex LED board is designed for 2.0 - 5.2V!
+            Higher voltages can damage the LEDs.
+ Arduino Pin | Charlieplex Board
+ ------------|------------------
+      2      |         A
+      3      |         B
+      4      |         C
+      5      |         D
+      6      |         E
+      7      |         F
+      8      |         G
+      9      |         H
+Resources:
+Include Chaplex.h, SparkFun_LED_8x7.h
+The Chaplex library can be found at: 
+http://playground.arduino.cc/Code/Chaplex
+Development environment specifics:
+Written in Arduino 1.6.7
+Tested with SparkFun RedBoard and BadgerStick (Interactive Badge)
+This code is released under the [MIT License](http://opensource.org/licenses/MIT).
+Please review the LICENSE.md file included with this example. If you have any questions 
+or concerns with licensing, please contact techsupport@sparkfun.com.
+Distributed as-is; no warranty is given.
+****************************************************************/
+
+#include <SparkFun_LED_8x7.h>
+#include <Chaplex.h>
+
+byte led_pins[] = {2, 3, 4, 5, 6, 7, 8, 9}; // Pins for LEDs
+byte i;
+byte x;
+byte y;
+byte radius;
+const byte sparkfun_logo[] = { 0,0,0,0,1,0,0,0,
+                               0,0,0,0,1,0,1,0,
+                               0,0,1,0,1,1,1,0,
+                               0,0,1,1,1,1,1,0,
+                               0,0,1,1,1,1,0,0,
+                               0,0,1,1,0,0,0,0,
+                               0,0,1,0,0,0,0,0 };
+
+void setup() {
+  Serial.begin(9600);
+  Serial.println(F("-----------------------"));
+  Serial.println(F("SparkFun LED 8x7 - Demo"));
+  Serial.println(F("-----------------------"));
+
+  // Initialize and clear display
+  Plex.init(led_pins);
+  Plex.clear();
+  Plex.display();
+  
+  // Seed our random number generator with an analog voltage read
+  randomSeed(analogRead(0));
+
+}
+
+void loop() {
+
+  // Show a bitmap
+  Serial.println(F("Draw a bitmap"));
+  Plex.clear();
+  Plex.drawBitmap(sparkfun_logo);
+  Plex.display();
+  delay(2000);
+  
+  // Scroll some text
+  Serial.println(F("Scroll some text"));
+  // You can replace the text below with whatever you want
+  Plex.scrollText("KK6OOS @ DefCon 24", 1);
+  // Make sure you adjust the delay otherwise the text will be cut off
+  delay(9500);
+
+  // Explosions!
+  Serial.println(F("Display an animation"));
+  for ( i = 0; i < 15; i++ ) {
+    x = random(0, 8);
+    y = random(0, 7);
+    for ( radius = 0; radius < 12; radius++ ) {
+      Plex.clear();
+      Plex.circle(x, y, radius);
+      Plex.circle(x, y, radius + 1);
+      Plex.display();
+      delay(30);
+    }
+  }
+}
